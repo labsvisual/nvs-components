577a2e658539a08aef6d9202deaf0931564a6024
Shreyansh Pandey <pandey@uw.edu>
2018-10-11 18:44:25 +0800

    added multi-select

--- KitchenSink/Form1.Designer.cs
+++ KitchenSink/Form1.Designer.cs
@@ -28,39 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Node0");
-            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Node4");
-            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Node1", new System.Windows.Forms.TreeNode[] {
-            treeNode2});
-            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Node5");
-            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Node2", new System.Windows.Forms.TreeNode[] {
-            treeNode4});
-            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Node3");
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
             this.nvsTreeView1 = new NVS.Components.NVSTreeView();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 428);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "NVS.TreeView";
             // 
             // nvsTreeView1
             // 
             this.nvsTreeView1.Location = new System.Drawing.Point(12, 12);
             this.nvsTreeView1.Name = "nvsTreeView1";
-            treeNode1.Name = "Node0";
-            treeNode1.Text = "Node0";
-            treeNode2.Name = "Node4";
-            treeNode2.Text = "Node4";
-            treeNode3.Name = "Node1";
-            treeNode3.Text = "Node1";
-            treeNode4.Name = "Node5";
-            treeNode4.Text = "Node5";
-            treeNode5.Name = "Node2";
-            treeNode5.Text = "Node2";
-            treeNode6.Name = "Node3";
-            treeNode6.Text = "Node3";
-            this.nvsTreeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode1,
-            treeNode3,
-            treeNode5,
-            treeNode6});
-            this.nvsTreeView1.Size = new System.Drawing.Size(776, 426);
+            this.nvsTreeView1.SelectedNodes = ((System.Collections.Generic.List<System.Windows.Forms.TreeNode>)(resources.GetObject("nvsTreeView1.SelectedNodes")));
+            this.nvsTreeView1.Size = new System.Drawing.Size(776, 413);
+            this.nvsTreeView1.StartingDirectory = "C:\\Users\\Shreyansh Pandey\\Desktop";
             this.nvsTreeView1.TabIndex = 0;
             // 
             // Form1
@@ -68,16 +56,19 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.nvsTreeView1);
             this.Name = "Form1";
             this.Text = "Kitchen Sink";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private NVS.Components.NVSTreeView nvsTreeView1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- NVS.Components/Components/NVSTreeView.cs
+++ NVS.Components/Components/NVSTreeView.cs
@@ -7,6 +7,7 @@ using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using System.IO;
 using System.ComponentModel;
+using System.Drawing;
 
 namespace NVS.Components
 {
@@ -21,6 +22,507 @@ namespace NVS.Components
         [DllImport("uxtheme.dll", ExactSpelling = true, CharSet = CharSet.Unicode)]
         private static extern int SetWindowTheme(IntPtr hwnd, string pszSubAppName, string pszSubIdList);
 
+        #region Overridden Properties
+
+        private List<TreeNode> selectedNodes = null;
+        private TreeNode selectedNode = null;
+
+        /// <summary>
+        /// Gets the list of selected node in the current context.
+        /// </summary>
+        public List<TreeNode> SelectedNodes
+        {
+            get
+            {
+                return selectedNodes;
+            } 
+
+            set
+            {
+                ClearSelectedNodes();
+
+                if(value != null)
+                {
+                    foreach(TreeNode node in selectedNodes)
+                    {
+                        ToggleNode(node, true);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the currently selected node.
+        /// </summary>
+        public new TreeNode SelectedNode
+        {
+            get
+            {
+                return selectedNode;
+            }
+            set
+            {
+                ClearSelectedNodes();
+
+                if(value != null)
+                {
+                    SelectNode(value);
+                }
+            }
+        }
+
+        #endregion
+
+        #region Overridden Events
+
+        protected override void OnGotFocus(EventArgs e)
+        {
+            try
+            {
+                if ( selectedNode == null && this.TopNode != null )
+                {
+                    ToggleNode(this.TopNode, true);
+                }
+
+                base.OnGotFocus(e);
+            } catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            try
+            {
+                base.SelectedNode = null;
+
+                TreeNode node = this.GetNodeAt(e.Location);
+                if(node != null)
+                {
+                    int leftOffset = node.Bounds.X;
+                    int rightOffset = node.Bounds.Right + 10;
+
+                    if(e.Location.X > leftOffset && e.Location.Y < rightOffset)
+                    {
+                        if (ModifierKeys == Keys.None && selectedNodes.Contains(node)) { }
+                        else
+                        {
+                            SelectNode(node);
+                        }
+                    }
+                }
+
+                base.OnMouseDown(e);
+            } catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            try
+            {
+                base.SelectedNode = null;
+
+                TreeNode node = this.GetNodeAt(e.Location);
+                if (node != null)
+                {
+                    int leftOffset = node.Bounds.X;
+                    int rightOffset = node.Bounds.Right + 10;
+
+                    if (e.Location.X > leftOffset && e.Location.Y < rightOffset)
+                    {
+                        if (ModifierKeys == Keys.None && selectedNodes.Contains(node))
+                        {
+                            SelectNode(node);
+                        }
+                    }
+                }
+
+                base.OnMouseUp(e);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        protected override void OnItemDrag(ItemDragEventArgs e)
+        {
+            try
+            {
+                TreeNode node = e.Item as TreeNode;
+
+                if (node != null)
+                {
+                    if (!selectedNodes.Contains(node))
+                    {
+                        SelectSingleNode(node);
+                        ToggleNode(node, true);
+                    }
+                }
+
+                base.OnItemDrag(e);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        protected override void OnBeforeSelect(TreeViewCancelEventArgs e)
+        {
+            try
+            {
+                base.SelectedNode = null;
+                e.Cancel = true;
+
+                base.OnBeforeSelect(e);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        protected override void OnAfterSelect(TreeViewEventArgs e)
+        {
+            try
+            {
+                base.OnAfterSelect(e);
+                base.SelectedNode = null;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+            if (e.KeyCode == Keys.ShiftKey) return;
+           
+            bool bShift = (ModifierKeys == Keys.Shift);
+
+            try
+            {
+                if (selectedNodes == null && this.TopNode != null)
+                {
+                    ToggleNode(this.TopNode, true);
+                }
+                
+                if (selectedNodes == null) return;
+
+                if (e.KeyCode == Keys.Left)
+                {
+                    if (selectedNode.IsExpanded && selectedNode.Nodes.Count > 0)
+                    {
+                        selectedNode.Collapse();
+                    }
+                    else if (selectedNode.Parent != null)
+                    {
+                        SelectSingleNode(selectedNode.Parent);
+                    }
+                }
+                else if (e.KeyCode == Keys.Right)
+                {
+                    if (!selectedNode.IsExpanded)
+                    {
+                        selectedNode.Expand();
+                    }
+                    else
+                    {
+                        SelectSingleNode(selectedNode.FirstNode);
+                    }
+                }
+                else if (e.KeyCode == Keys.Up)
+                {
+                    if (selectedNode.PrevVisibleNode != null)
+                    {
+                        SelectNode(selectedNode.PrevVisibleNode);
+                    }
+                }
+                else if (e.KeyCode == Keys.Down)
+                {
+                    if (selectedNode.NextVisibleNode != null)
+                    {
+                        SelectNode(selectedNode.NextVisibleNode);
+                    }
+                }
+                else if (e.KeyCode == Keys.Home)
+                {
+                    if (bShift)
+                    {
+                        if (selectedNode.Parent == null)
+                        {
+                            if (this.Nodes.Count > 0)
+                            {
+                                SelectNode(this.Nodes[0]);
+                            }
+                        }
+                        else
+                        {
+                            SelectNode(selectedNode.Parent.FirstNode);
+                        }
+                    }
+                    else
+                    {
+                        if (this.Nodes.Count > 0)
+                        {
+                            SelectSingleNode(this.Nodes[0]);
+                        }
+                    }
+                }
+                else if (e.KeyCode == Keys.End)
+                {
+                    if (bShift)
+                    {
+                        if (selectedNode.Parent == null)
+                        {
+                            if (this.Nodes.Count > 0)
+                            {
+                                SelectNode(this.Nodes[this.Nodes.Count - 1]);
+                            }
+                        }
+                        else
+                        {
+                            SelectNode(selectedNode.Parent.LastNode);
+                        }
+                    }
+                    else
+                    {
+                        if (this.Nodes.Count > 0)
+                        {
+                            TreeNode ndLast = this.Nodes[0].LastNode;
+                            while (ndLast.IsExpanded && (ndLast.LastNode != null))
+                            {
+                                ndLast = ndLast.LastNode;
+                            }
+                            SelectSingleNode(ndLast);
+                        }
+                    }
+                }
+                else if (e.KeyCode == Keys.PageUp)
+                {
+                    int nCount = this.VisibleCount;
+                    TreeNode ndCurrent = selectedNode;
+                    while ((nCount) > 0 && (ndCurrent.PrevVisibleNode != null))
+                    {
+                        ndCurrent = ndCurrent.PrevVisibleNode;
+                        nCount--;
+                    }
+                    SelectSingleNode(ndCurrent);
+                }
+                else if (e.KeyCode == Keys.PageDown)
+                {
+                    int nCount = this.VisibleCount;
+                    TreeNode ndCurrent = selectedNode;
+                    while ((nCount) > 0 && (ndCurrent.NextVisibleNode != null))
+                    {
+                        ndCurrent = ndCurrent.NextVisibleNode;
+                        nCount--;
+                    }
+                    SelectSingleNode(ndCurrent);
+                }
+                else
+                {
+                    string sSearch = ((char)e.KeyValue).ToString();
+
+                    TreeNode ndCurrent = selectedNode;
+                    while ((ndCurrent.NextVisibleNode != null))
+                    {
+                        ndCurrent = ndCurrent.NextVisibleNode;
+                        if (ndCurrent.Text.StartsWith(sSearch))
+                        {
+                            SelectSingleNode(ndCurrent);
+                            break;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                this.EndUpdate();
+            }
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        private void SelectNode(TreeNode node)
+        {
+            try
+            {
+                this.BeginUpdate();
+
+                if (selectedNode == null || ModifierKeys == Keys.Control)
+                {
+                    bool bIsSelected = selectedNodes.Contains(node);
+                    ToggleNode(node, !bIsSelected);
+                }
+                else if (ModifierKeys == Keys.Shift)
+                {
+                    TreeNode ndStart = selectedNode;
+                    TreeNode ndEnd = node;
+
+                    if (ndStart.Parent == ndEnd.Parent)
+                    {
+                        if (ndStart.Index < ndEnd.Index)
+                        {
+                            while (ndStart != ndEnd)
+                            {
+                                ndStart = ndStart.NextVisibleNode;
+                                if (ndStart == null) break;
+                                ToggleNode(ndStart, true);
+                            }
+                        }
+                        else if (ndStart.Index == ndEnd.Index) { }
+                        else
+                        {
+                            while (ndStart != ndEnd)
+                            {
+                                ndStart = ndStart.PrevVisibleNode;
+                                if (ndStart == null) break;
+                                ToggleNode(ndStart, true);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        TreeNode ndStartP = ndStart;
+                        TreeNode ndEndP = ndEnd;
+                        int startDepth = Math.Min(ndStartP.Level, ndEndP.Level);
+                        
+                        while (ndStartP.Level > startDepth)
+                        {
+                            ndStartP = ndStartP.Parent;
+                        }
+                        
+                        while (ndEndP.Level > startDepth)
+                        {
+                            ndEndP = ndEndP.Parent;
+                        }
+                        
+                        while (ndStartP.Parent != ndEndP.Parent)
+                        {
+                            ndStartP = ndStartP.Parent;
+                            ndEndP = ndEndP.Parent;
+                        }
+                        
+                        if (ndStartP.Index < ndEndP.Index)
+                        {
+                            while (ndStart != ndEnd)
+                            {
+                                ndStart = ndStart.NextVisibleNode;
+                                if (ndStart == null) break;
+                                ToggleNode(ndStart, true);
+                            }
+                        }
+                        else if (ndStartP.Index == ndEndP.Index)
+                        {
+                            if (ndStart.Level < ndEnd.Level)
+                            {
+                                while (ndStart != ndEnd)
+                                {
+                                    ndStart = ndStart.NextVisibleNode;
+                                    if (ndStart == null) break;
+                                    ToggleNode(ndStart, true);
+                                }
+                            }
+                            else
+                            {
+                                while (ndStart != ndEnd)
+                                {
+                                    ndStart = ndStart.PrevVisibleNode;
+                                    if (ndStart == null) break;
+                                    ToggleNode(ndStart, true);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            while (ndStart != ndEnd)
+                            {
+                                ndStart = ndStart.PrevVisibleNode;
+                                if (ndStart == null) break;
+                                ToggleNode(ndStart, true);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    SelectSingleNode(node);
+                }
+
+                OnAfterSelect(new TreeViewEventArgs(selectedNode));
+            }
+            finally
+            {
+                this.EndUpdate();
+            }
+        }
+
+        private void ClearSelectedNodes()
+        {
+            try
+            {
+                foreach (TreeNode node in selectedNodes)
+                {
+                    node.BackColor = this.BackColor;
+                    node.ForeColor = this.ForeColor;
+                }
+            }
+            finally
+            {
+                selectedNodes.Clear();
+                selectedNode = null;
+            }
+        }
+
+        private void SelectSingleNode(TreeNode node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            ClearSelectedNodes();
+            ToggleNode(node, true);
+            node.EnsureVisible();
+        }
+
+        private void ToggleNode(TreeNode node, bool bSelectNode)
+        {
+            if (bSelectNode)
+            {
+                selectedNode = node;
+                if (!selectedNodes.Contains(node))
+                {
+                    selectedNodes.Add(node);
+                }
+                node.BackColor = SystemColors.Highlight;
+                node.ForeColor = SystemColors.HighlightText;
+            }
+            else
+            {
+                selectedNodes.Remove(node);
+                node.BackColor = this.BackColor;
+                node.ForeColor = this.ForeColor;
+            }
+        }
+        
+
+        #endregion
+
         protected override void OnHandleCreated(EventArgs e)
         {
             base.OnHandleCreated(e);
@@ -31,6 +533,9 @@ namespace NVS.Components
         {
             this.StartingDirectory = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
             this.PopulateTreeView();
+
+            selectedNodes = new List<TreeNode>();
+            base.SelectedNode = null;
         }
 
         /// <summary>
