8bac1d3529b372ab7f7c8a738bd6432e5850687f
Shreyansh Pandey <pandey@uw.edu>
2018-10-12 17:08:34 +0800

    added NVSButton

--- KitchenSink/Form1.Designer.cs
+++ KitchenSink/Form1.Designer.cs
@@ -28,8 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("desktop.ini");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("EzLicense-Licensing.ezproj");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("jjj.xps");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("KryptonHelp.chm");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("license1.lic");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("LicenseManager.sdf");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("macd-cleaned.exe");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("macd.exe");
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("MalwareBytes_Anti-Malware_Keygen_v1.7_URET.exe");
+            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("new-test.xml");
+            System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("Red Gate Keygen.exe");
+            System.Windows.Forms.TreeNode treeNode12 = new System.Windows.Forms.TreeNode("run.cmd");
+            System.Windows.Forms.TreeNode treeNode13 = new System.Windows.Forms.TreeNode("test.ezproj");
+            System.Windows.Forms.TreeNode treeNode14 = new System.Windows.Forms.TreeNode("test.xml");
+            System.Windows.Forms.TreeNode treeNode15 = new System.Windows.Forms.TreeNode("VCSExpress.exe");
+            System.Windows.Forms.TreeNode treeNode16 = new System.Windows.Forms.TreeNode("vs_enterprise__1986905726.1536119538.exe");
+            System.Windows.Forms.TreeNode treeNode17 = new System.Windows.Forms.TreeNode("Desktop", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3,
+            treeNode4,
+            treeNode5,
+            treeNode6,
+            treeNode7,
+            treeNode8,
+            treeNode9,
+            treeNode10,
+            treeNode11,
+            treeNode12,
+            treeNode13,
+            treeNode14,
+            treeNode15,
+            treeNode16});
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
+            this.nvsButton1 = new NVS.Components.NVSButton();
             this.nvsTreeView1 = new NVS.Components.NVSTreeView();
             this.SuspendLayout();
             // 
@@ -42,12 +76,62 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "NVS.TreeView";
             // 
+            // nvsButton1
+            // 
+            this.nvsButton1.BackColor = System.Drawing.Color.Black;
+            this.nvsButton1.BorderRadius = 10;
+            this.nvsButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.nvsButton1.FontSize = 15F;
+            this.nvsButton1.ForeColor = System.Drawing.SystemColors.Control;
+            this.nvsButton1.Location = new System.Drawing.Point(95, 12);
+            this.nvsButton1.Name = "nvsButton1";
+            this.nvsButton1.Size = new System.Drawing.Size(198, 91);
+            this.nvsButton1.TabIndex = 2;
+            this.nvsButton1.Text = "Hello World";
+            this.nvsButton1.UseVisualStyleBackColor = true;
+            // 
             // nvsTreeView1
             // 
             this.nvsTreeView1.Location = new System.Drawing.Point(12, 12);
             this.nvsTreeView1.Name = "nvsTreeView1";
+            treeNode1.Name = "";
+            treeNode1.Text = "desktop.ini";
+            treeNode2.Name = "";
+            treeNode2.Text = "EzLicense-Licensing.ezproj";
+            treeNode3.Name = "";
+            treeNode3.Text = "jjj.xps";
+            treeNode4.Name = "";
+            treeNode4.Text = "KryptonHelp.chm";
+            treeNode5.Name = "";
+            treeNode5.Text = "license1.lic";
+            treeNode6.Name = "";
+            treeNode6.Text = "LicenseManager.sdf";
+            treeNode7.Name = "";
+            treeNode7.Text = "macd-cleaned.exe";
+            treeNode8.Name = "";
+            treeNode8.Text = "macd.exe";
+            treeNode9.Name = "";
+            treeNode9.Text = "MalwareBytes_Anti-Malware_Keygen_v1.7_URET.exe";
+            treeNode10.Name = "";
+            treeNode10.Text = "new-test.xml";
+            treeNode11.Name = "";
+            treeNode11.Text = "Red Gate Keygen.exe";
+            treeNode12.Name = "";
+            treeNode12.Text = "run.cmd";
+            treeNode13.Name = "";
+            treeNode13.Text = "test.ezproj";
+            treeNode14.Name = "";
+            treeNode14.Text = "test.xml";
+            treeNode15.Name = "";
+            treeNode15.Text = "VCSExpress.exe";
+            treeNode16.Name = "";
+            treeNode16.Text = "vs_enterprise__1986905726.1536119538.exe";
+            treeNode17.Name = "";
+            treeNode17.Text = "Desktop";
+            this.nvsTreeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode17});
             this.nvsTreeView1.SelectedNodes = ((System.Collections.Generic.List<System.Windows.Forms.TreeNode>)(resources.GetObject("nvsTreeView1.SelectedNodes")));
-            this.nvsTreeView1.Size = new System.Drawing.Size(776, 413);
+            this.nvsTreeView1.Size = new System.Drawing.Size(77, 413);
             this.nvsTreeView1.StartingDirectory = "C:\\Users\\Shreyansh Pandey\\Desktop";
             this.nvsTreeView1.TabIndex = 0;
             // 
@@ -56,6 +140,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.nvsButton1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.nvsTreeView1);
             this.Name = "Form1";
@@ -69,6 +154,7 @@
 
         private NVS.Components.NVSTreeView nvsTreeView1;
         private System.Windows.Forms.Label label1;
+        private NVS.Components.NVSButton nvsButton1;
     }
 }
 
---
+++ NVS.Components/Components/NVSButton.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using NVS.Extensions;
+using System.ComponentModel;
+using System.Drawing.Text;
+using System.IO;
+
+namespace NVS.Components
+{
+    /// <summary>
+    /// Represents a custom-designed button.
+    /// </summary>
+    public class NVSButton : Button
+    {
+
+        private PrivateFontCollection _privateFontCollection = new PrivateFontCollection();
+        private string _fontFolderPath;
+        private string _fontFilePath;
+        private Color _primaryBaseColor = ColorTranslator.FromHtml("#212020");
+        private Color _currentBaseColor;
+
+        /// <summary>
+        /// The radius of the button.
+        /// </summary>
+        [Description("Gets or sets the radius of the button")]
+        [Category("Appearance")]
+        public int BorderRadius { get; set; } = 10;
+
+        /// <summary>
+        /// The font size of the button.
+        /// </summary>
+        [Description("The font size of the text on the button")]
+        [Category("Appearance")]
+        public float FontSize { get; set; } = 14.0f;
+
+        private GraphicsPath GetRoundedPath(RectangleF rect, int radius)
+        {
+            GraphicsPath gPath = new GraphicsPath();
+
+            float fixedRadius = radius / 2;
+
+            gPath.StartFigure();
+
+            // Top left to top right
+            gPath.AddArc(rect.X, rect.Y, radius, radius, 180, 90);
+            gPath.AddLine(rect.X + fixedRadius, rect.Y, rect.Width - fixedRadius, rect.Y);
+
+            // Top right to bottom right
+            gPath.AddArc(rect.X + rect.Width - radius, rect.Y, radius, radius, 270, 90);
+            gPath.AddLine(rect.Width, rect.Y + fixedRadius, rect.Width, rect.Height - fixedRadius);
+
+            // Bottom right to bottom left
+            gPath.AddArc(rect.X + rect.Width - radius, rect.Y + rect.Height - radius, radius, radius, 0, 90);
+            gPath.AddLine(rect.Width - fixedRadius, rect.Height, rect.X + fixedRadius, rect.Height);
+
+            // Bottom left to top left
+            gPath.AddArc(rect.X, rect.Y + rect.Height - radius, radius, radius, 90, 90);
+            gPath.AddLine(rect.X, rect.Height - fixedRadius, rect.X, rect.Y + fixedRadius);
+
+            gPath.CloseFigure();
+
+            return gPath;
+        }
+
+        protected override void OnMouseEnter(EventArgs e)
+        {
+            base.OnMouseEnter(e);
+
+            this._currentBaseColor = ColorTranslator.FromHtml("#2D2D2D");
+            Invalidate();
+        }
+
+        protected override void OnMouseLeave(EventArgs e)
+        {
+            base.OnMouseLeave(e);
+
+            this._currentBaseColor = this._primaryBaseColor;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            RectangleF rect = new RectangleF(0, 0, this.Bounds.Width, this.Bounds.Height);
+            GraphicsPath gPath = GetRoundedPath(rect, 25);
+
+            this.Region = new Region(gPath);
+            using(Pen p = new Pen(this._currentBaseColor, 1.75f))
+            {
+                p.Alignment = PenAlignment.Inset;
+                e.Graphics.DrawPath(p, gPath);
+            }
+
+            SolidBrush brush = new SolidBrush(this._currentBaseColor);
+            e.Graphics.FillRoundedRectangle(brush, 0, 0, this.Bounds.Width, this.Bounds.Height, this.BorderRadius);
+            
+            ControlPaint.DrawBorder(e.Graphics, this.ClientRectangle,
+                Color.Transparent, 0, ButtonBorderStyle.None, // left
+                Color.Transparent, 0, ButtonBorderStyle.None, // top
+                Color.Transparent, 0, ButtonBorderStyle.None, // right
+                Color.Black, 10, ButtonBorderStyle.Inset);    // bottom
+
+            FontFamily fFamily = _privateFontCollection.Families[0];
+            Font font = new Font(fFamily.Name, this.FontSize);
+            StringFormat stringFormat = new StringFormat();
+            stringFormat.LineAlignment = StringAlignment.Center;
+            stringFormat.Alignment = StringAlignment.Center;
+
+            e.Graphics.DrawString(this.Text.ToUpper(), font, new SolidBrush(Color.White), this.ClientRectangle, stringFormat);
+            
+        }
+
+        private void LoadFonts()
+        {
+            this._fontFolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "NVS");
+            this._fontFilePath = Path.Combine(this._fontFolderPath, "DroidSans.ttf");
+
+            if (!Directory.Exists(this._fontFolderPath))
+            {
+                try
+                {
+                    Directory.CreateDirectory(this._fontFolderPath);
+                } catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+
+            if (!File.Exists(this._fontFilePath))
+            {
+                File.WriteAllBytes(this._fontFilePath, Fonts.DroidSans_Bold);
+            }
+
+            _privateFontCollection.AddFontFile(this._fontFilePath);
+         
+        }
+
+        public NVSButton()
+        {
+            this.FlatStyle = FlatStyle.Flat;
+            this.BackColor = Color.Black;
+            this.DoubleBuffered = true;
+            this.ResizeRedraw = true;
+
+            this._currentBaseColor = this._primaryBaseColor;
+
+            LoadFonts();
+        }
+    }
+}
